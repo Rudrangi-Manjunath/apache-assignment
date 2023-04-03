3033ef71f90e26c18a87af35649f869a4e79d05c
Rudrangi Manjunath <rudrangimanjunath@gmail.com>
2023-04-03 20:16:47 +0530

    modified csv file reading

--- app/src/main/java/assignment/Practise.java
+++ app/src/main/java/assignment/Practise.java
@@ -1,6 +1,7 @@
 package assignment;
 
 import java.io.IOException;
+
 import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,9 +10,9 @@ import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.jdbc.JdbcIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.options.PipelineOptions.DirectRunner;
 import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
@@ -20,7 +21,6 @@ import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionTuple;
 import org.apache.beam.sdk.values.TupleTag;
 import org.apache.beam.sdk.values.TupleTagList;
-
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 
@@ -76,31 +76,43 @@ public class Practise {
         Pipeline pipeline = Pipeline.create(options);
         Schema schema = new Schema.Parser().parse(schemaString);
 
-        CSVFormat csvFormat = CSVFormat.DEFAULT.withSkipHeaderRecord().withDelimiter(',');
-        CSVProcessor csvProcessor = new CSVProcessor(options.getCSVFilePath(), csvFormat);
+        PCollection<String> lines = pipeline.apply("ReadLines", TextIO.read().from(options.getCSVFilePath()));
 
-        List<CSVRecord> records = csvProcessor.parse();
+        PCollection<CSVRecord> records = lines.apply(ParDo.of(new DoFn<String, CSVRecord>() {
+            @ProcessElement
+            public void processElement(ProcessContext c) throws IOException {
+                String line = c.element();
+                CSVFormat format = CSVFormat.DEFAULT.withHeader().withDelimiter(',');
+                CSVRecord record = CSVProcessor.process(line, format);
+                c.output(record);
+            }
+        }));
 
         List<GenericRecord> genericRecords = new ArrayList<>();
-        for (CSVRecord record : records) {
-            GenericRecord genericRecord = new GenericData.Record(schema);
-            genericRecord.put("building_code", record.get("building_code"));
-            genericRecord.put("equipment_code", record.get("equipment_code"));
-            genericRecord.put("datetime", record.get("datetime"));
-            genericRecord.put("timezone", record.get("timezone"));
-            genericRecord.put("event_type", record.get("event_type"));
-            genericRecord.put("direction", record.get("direction"));
-            genericRecord.put("card_id", record.get("card_id"));
-            genericRecord.put("person_type", record.get("person_type"));
-            genericRecord.put("team", record.get("team"));
-            genericRecord.put("event_id", record.get("event_id"));
-            genericRecord.put("pdm_job_id", record.get("pdm_job_id"));
-            genericRecord.put("filename", record.get("filename"));
-            genericRecord.put("business_unit_code", record.get("business_unit_code"));
-            genericRecord.put("allocated_space_code", record.get("allocated_space_code"));
-            genericRecord.put("career_level_code", record.get("career_level_code"));
-            genericRecords.add(genericRecord);
-        }
+        records.apply(ParDo.of(new DoFn<CSVRecord, GenericRecord>() {
+            @ProcessElement
+            public void processElement(ProcessContext c) {
+                CSVRecord record = c.element();
+                GenericRecord genericRecord = new GenericData.Record(schema);
+                genericRecord.put("building_code", record.get("building_code"));
+                genericRecord.put("equipment_code", record.get("equipment_code"));
+                genericRecord.put("datetime", record.get("datetime"));
+                genericRecord.put("timezone", record.get("timezone"));
+                genericRecord.put("event_type", record.get("event_type"));
+                genericRecord.put("direction", record.get("direction"));
+                genericRecord.put("card_id", record.get("card_id"));
+                genericRecord.put("person_type", record.get("person_type"));
+                genericRecord.put("team", record.get("team"));
+                genericRecord.put("event_id", record.get("event_id"));
+                genericRecord.put("pdm_job_id", record.get("pdm_job_id"));
+                genericRecord.put("filename", record.get("filename"));
+                genericRecord.put("business_unit_code", record.get("business_unit_code"));
+                genericRecord.put("allocated_space_code", record.get("allocated_space_code"));
+                genericRecord.put("career_level_code", record.get("career_level_code"));
+                c.output(genericRecord);
+            }
+        }));
+
         TupleTag<GenericRecord> goodRecords = new TupleTag<GenericRecord>() {
         };
         TupleTag<GenericRecord> badRecords = new TupleTag<GenericRecord>() {
@@ -154,6 +166,7 @@ public class Practise {
         PCollection<Long> good_records_count = goodCollection.apply(Count.globally());
         PCollection<GenericRecord> badCollection = categorizedRecords.get(badRecords);
         PCollection<Long> bad_records_count = badCollection.apply(Count.globally());
+
         pipeline.run();
     }
 }
