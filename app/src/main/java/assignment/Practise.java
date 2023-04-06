1eca7bd049304972d74be8d66dccbc3966bc409d
Rudrangi Manjunath <rudrangimanjunath@gmail.com>
2023-04-06 13:00:19 +0530

    adding whole assignment

--- app/src/main/java/assignment/Practise.java
+++ app/src/main/java/assignment/Practise.java
@@ -3,6 +3,8 @@ package assignment;
 import java.io.IOException;
 
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Map;
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
@@ -13,9 +15,11 @@ import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.jdbc.JdbcIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.View;
+import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionTuple;
 import org.apache.beam.sdk.values.PCollectionView;
@@ -27,21 +31,20 @@ import org.apache.commons.csv.CSVRecord;
 public class Practise {
 
     private static boolean isValidRecord(GenericRecord record) {
-        if (record.get("building_code") == null || record.get("building_code").toString().isEmpty()) {
+        if (record == null) {
+            System.out.println("Record is null");
             return false;
-        } else if (record.get("equipment_code") == null || record.get("equipment_code").toString().isEmpty()) {
-            return false;
-        } else if (record.get("datetime") == null || record.get("datetime").toString().isEmpty()) {
-            return false;
-        } else if (record.get("timezone") == null || record.get("timezone").toString().isEmpty()) {
-            return false;
-        } else if (record.get("event_type") == null || record.get("event_type").toString().isEmpty()) {
-            return false;
-        } else if (record.get("card_id") == null || record.get("card_id").toString().isEmpty()) {
-            return false;
-        } else {
-            return true;
         }
+
+        String[] requiredFields = { "building_code", "equipment_code", "datetime", "timezone", "event_type",
+                "card_id" };
+        for (String field : requiredFields) {
+            Object value = record.get(field);
+            if (value.toString().isEmpty() || value.toString().equals("null")) {
+                return false;
+            }
+        }
+        return true;
 
     }
 
@@ -65,7 +68,6 @@ public class Practise {
             public void processElement(ProcessContext c) throws IOException {
                 String line = c.element();
                 CSVRecord record = CSVProcessor.process(line, csvFormat);
-                System.out.println(record);
                 GenericRecord genericRecord = new GenericData.Record(schema);
                 genericRecord.put("building_code", record.get("building_code"));
                 genericRecord.put("equipment_code", record.get("equipment_code"));
@@ -83,7 +85,7 @@ public class Practise {
                 genericRecord.put("allocated_space_code", record.get("allocated_space_code"));
                 genericRecord.put("career_level_code", record.get("career_level_code"));
                 c.output(genericRecord);
-                System.out.println(genericRecord);
+
             }
         })).setCoder(AvroCoder.of(schema));
 
@@ -136,8 +138,56 @@ public class Practise {
                 }));
 
         PCollection<GenericRecord> badCollection = categorizedRecords.get(badRecords);
-        PCollection<Long> bad_records = badCollection.apply(Count.globally());
         PCollection<Long> good_records_count = goodCollection.apply(Count.globally());
+        PCollection<Long> bad_records_count = badCollection.apply(Count.globally());
+
+        PCollection<KV<String, Long>> good_records_count_kv = good_records_count
+                .apply(ParDo.of(new DoFn<Long, KV<String, Long>>() {
+                    @ProcessElement
+                    public void processElement(ProcessContext c) {
+                        c.output(KV.of("good_records_count", c.element()));
+                    }
+                }));
+
+        PCollection<KV<String, Long>> bad_records_count_kv = bad_records_count
+                .apply(ParDo.of(new DoFn<Long, KV<String, Long>>() {
+                    @ProcessElement
+                    public void processElement(ProcessContext c) {
+                        c.output(KV.of("bad_records_count", c.element()));
+                    }
+                }));
+
+        PCollectionView<Map<String, Long>> good_records_count_view = good_records_count_kv.apply(View.asMap());
+        PCollectionView<Map<String, Long>> bad_records_count_view = bad_records_count_kv.apply(View.asMap());
+
+        PCollection<String> result = pipeline.apply(Create.of("dummy")).apply(ParDo.of(new DoFn<String, String>() {
+            @ProcessElement
+            public void processElement(ProcessContext c) {
+                Map<String, Long> good_records_count_map = c.sideInput(good_records_count_view);
+                Map<String, Long> bad_records_count_map = c.sideInput(bad_records_count_view);
+                String result = "good_records_count: " + good_records_count_map.get("good_records_count")
+                        + ", bad_records_count: " + bad_records_count_map.get("bad_records_count");
+                c.output(result);
+            }
+        }).withSideInputs(good_records_count_view, bad_records_count_view));
+
+        result.apply(JdbcIO.<String>write()
+                .withDataSourceConfiguration(JdbcIO.DataSourceConfiguration
+                        .create("org.postgresql.Driver", options.getdbUrl())
+                        .withUsername(options.getdbUserName())
+                        .withPassword(options.getdbPassword()))
+                .withStatement(
+                        "INSERT INTO job(job_date,good_record_count,error_record_count) VALUES (?,?,?)")
+                .withPreparedStatementSetter(new JdbcIO.PreparedStatementSetter<String>() {
+                    public void setParameters(String element, PreparedStatement query) throws SQLException {
+                        String[] result = element.split(",");
+                        long goodRecordCount = Long.parseLong(result[0].split(":")[1].trim());
+                        long badRecordCount = Long.parseLong(result[1].split(":")[1].trim());
+                        query.setDate(1, new java.sql.Date(System.currentTimeMillis()));
+                        query.setLong(2, goodRecordCount);
+                        query.setLong(3, badRecordCount);
+                    }
+                }));
 
         pipeline.run();
     }
